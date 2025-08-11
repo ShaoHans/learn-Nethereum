10d9adc9cedc525b7ab6f55b5fa37ed0229b6cd5
Hans <shaomenghao@hotmail.com>
2025-08-11 17:41:42 +0800

    HDWallet

---
+++ HDWallet/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Nethereum.HdWallet;
+using Nethereum.Web3;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddUserSecrets<Program>()
+    .Build();
+
+var wallet = new Wallet(configuration["Ethereum:Words"], "");
+var account = wallet.GetAccount(1);
+Console.WriteLine(account.Address);
+var web3 = new Web3(account, configuration["Ethereum:NodeUrl"]);
+var balance = await web3.Eth.GetBalance.SendRequestAsync(account.Address);
+Console.WriteLine(balance);
