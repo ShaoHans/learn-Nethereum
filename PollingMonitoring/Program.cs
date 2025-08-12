bd34f0c876ca78a2a6dbcd6934d18d9b7bf4e347
Hans <shaomenghao@hotmail.com>
2025-08-12 10:54:53 +0800

    监听

---
+++ PollingMonitoring/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PollingMonitoring;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(
+        (context, config) =>
+        {
+            config
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddUserSecrets<Program>();
+        }
+    )
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<TransactionMonitoringBgService>();
+    })
+    .Build();
+
+await host.RunAsync();
---
+++ PollingMonitoring/TransactionMonitoringBgService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace PollingMonitoring;
+
+internal class TransactionMonitoringBgService : BackgroundService
+{
+    public TransactionMonitoringBgService()
+    {
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        
+    }
+}
